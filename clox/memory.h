15624294bed42180fe0ca777c29f1857045c04f4
burchr04 <Christopher.Burbach@csgi.com>
2020-06-02 16:50:02 -0500

    Fixed compilation errors

--- clox/memory.h
+++ clox/memory.h
@@ -7,9 +7,10 @@
 
 #define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
-#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity) * 2)
+#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity)*2)
 
-#define GROW_ARRAY(previous, type, oldCount, count) (type*)reallocate(previous, sizeof(type) * (oldCount), sizeof(type) * (count))
+#define GROW_ARRAY(previous, type, oldCount, count) \
+	(type*)reallocate(previous, sizeof(type) * (oldCount), sizeof(type) * (count))
 
 #define FREE_ARRAY(type, pointer, oldCount) reallocate(pointer, sizeof(type) * (oldCount), 0)
 
