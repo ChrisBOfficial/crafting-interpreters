e8f0135883c9f0da5ca60467441822bdfcbd4419
burchr04 <Christopher.Burbach@csgi.com>
2020-06-05 14:29:35 -0500

    Final optimizations

--- clox/common.h
+++ clox/common.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define NAN_BOXING
 #define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
@@ -16,7 +17,7 @@
 #endif
 // In the book, we show them defined, but for working on them locally,
 // we don't want them to be
-// #undef DEBUG_PRINT_CODE
+#undef DEBUG_PRINT_CODE
 #undef DEBUG_TRACE_EXECUTION
 #undef DEBUG_STRESS_GC
 #undef DEBUG_LOG_GC
